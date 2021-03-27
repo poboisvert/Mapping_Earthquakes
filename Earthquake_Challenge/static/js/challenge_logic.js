2e0be3559b7b9e87d09a396e06e349b68aafa422
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-03-27 09:46:54 -0400

    Deliverable 2

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -35,7 +35,6 @@ let baseMaps = {
   Streets: streets,
   Satellite: satelliteStreets,
 };
-
 // 1. Add a 2nd layer group for the tectonic plate data.
 let allEarthquakes = new L.LayerGroup();
 let allTechtonics = new L.LayerGroup();
@@ -43,7 +42,7 @@ let allTechtonics = new L.LayerGroup();
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
   Earthquakes: allEarthquakes,
-  Techtonic: allTechtonics,
+  "Techtonic Plates": allTechtonics,
 };
 
 // Then we add a control to the map that will allow the user to change which
@@ -121,6 +120,29 @@ d3.json(
 
   // Then we add the earthquake layer to our map.
   allEarthquakes.addTo(map);
+
+// 3. Retrieve the major earthquake GeoJSON data >4.5 mag for the week.
+d3.json().then(function(data) {
+
+  // 4. Use the same style as the earthquake data.
+  
+  
+  // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
+  
+  
+  // 6. Use the function that determines the radius of the earthquake marker based on its magnitude.
+  
+  
+  // 7. Creating a GeoJSON layer with the retrieved data that adds a circle to the map 
+  // sets the style of the circle, and displays the magnitude and location of the earthquake
+  //  after the marker has been created and styled.
+  L.geoJson(, {
+      
+  });
+  // 8. Add the major earthquakes layer to the map.
+  
+  // 9. Close the braces and parentheses for the major earthquake data.
+  });
 
   // Here we create a legend control object.
   let legend = L.control({
